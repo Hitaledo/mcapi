8dc78147c2a117c8d4fc63027a1e66c2986443b7
Ramiro <ramiro.depaoli@gmail.com>
2023-11-25 20:54:51 -0300

    fix

--- src/main/java/com/hitaledo/mcapi/App.java
+++ src/main/java/com/hitaledo/mcapi/App.java
@@ -2,23 +2,11 @@ package com.hitaledo.mcapi;
 import java.io.File;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 public class App extends JavaPlugin {
     String pluginName = "Minecraft API";
-    File data;
-    FileConfiguration dataConfig;
     File config;
     @Override
     public void onEnable() {
-        data = new File(getDataFolder(), "data.yml");
-        dataConfig = YamlConfiguration.loadConfiguration(data);
-        try{
-            dataConfig.save(data);
-        }catch(Exception exeption){
-            exeption.printStackTrace();
-            getLogger().info(ChatColor.RED + "Error creating data file");
-        }
         config = new File(getDataFolder(), "config.yml");
         if (!config.exists()){
             saveDefaultConfig();
--- src/main/java/com/hitaledo/mcapi/Web.java
+++ src/main/java/com/hitaledo/mcapi/Web.java
@@ -1,20 +1,21 @@
 package com.hitaledo.mcapi;
-import spark.Spark;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import static spark.Spark.*;
 public class Web {
     public static void enable(App plugin) {
-        Integer port = 80;
-        FileConfiguration dataConfig = plugin.dataConfig;
-        if(!dataConfig.contains("port")){
+        Integer port;
+        FileConfiguration config = plugin.getConfig();
+        try {
+            port = config.getInt("Config.port");
+        } catch (Exception e) {
+            port = 80;
             plugin.getLogger().info(ChatColor.GREEN + "Port config not found. Using default value: " + port);
-        } else {
-            port = dataConfig.getInt("port");
         }
-        Spark.port(port);
-        Spark.get("/", (req, res) -> "Minecraft API!");
+        port(port);
+        get("/", (req, res) -> "Minecraft API!");
     }
     public static void disable() {
-        Spark.stop();
+        stop();
     }
 }
