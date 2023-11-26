d73bdb8df0cdb588f8a32d6c4d6e63181886bf1e
Ramiro <ramiro.depaoli@gmail.com>
2023-11-26 11:02:05 -0300

    send commands

--- src/main/java/com/hitaledo/mcapi/App.java
+++ src/main/java/com/hitaledo/mcapi/App.java
@@ -1,19 +1,23 @@
 package com.hitaledo.mcapi;
+
 import java.io.File;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
+
 public class App extends JavaPlugin {
     String pluginName = "Minecraft API";
     File config;
+
     @Override
     public void onEnable() {
         config = new File(getDataFolder(), "config.yml");
-        if (!config.exists()){
+        if (!config.exists()) {
             saveDefaultConfig();
         }
         Web.enable(this);
         getLogger().info(ChatColor.GREEN + pluginName + " has been enabled!");
     }
+
     @Override
     public void onDisable() {
         Web.disable();
--- src/main/java/com/hitaledo/mcapi/Web.java
+++ src/main/java/com/hitaledo/mcapi/Web.java
@@ -1,7 +1,11 @@
 package com.hitaledo.mcapi;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import static spark.Spark.*;
+
 public class Web {
     public static void enable(App plugin) {
         Integer port;
@@ -13,8 +17,19 @@ public class Web {
             plugin.getLogger().info(ChatColor.GREEN + "Port config not found. Using default value: " + port);
         }
         port(port);
-        get("/", (req, res) -> "Minecraft API!");
+        get("/", (req, res) -> "Minecraft API enabled!");
+        post("/command", (req, res) -> {
+            String command = req.queryParams("command");
+            ConsoleCommandSender console = plugin.getServer().getConsoleSender();
+            try {
+                Bukkit.dispatchCommand(console, command);
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        });
     }
+
     public static void disable() {
         stop();
     }
